70bf0f55538968c357cb14a43330f05962d64d24
Pugazhenthi K <puzpuzpugazh@gmail.com>
2026-02-16 19:57:48 +0530

    structure mod

---
+++ src/com/user/login/LandingPage.java
@@ -0,0 +1,10 @@
+package com.user.login;
+
+public class LandingPage {
+
+	public static void main(String[] args) {
+		// TODO based on user choice re-direct to login / New user register -> Account creation 
+
+	}
+
+}
