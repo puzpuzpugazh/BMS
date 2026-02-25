97beb229be41e4c54f05d3eaa8d89fc979f03a3d
Jayasurya009 <jayasurya2k19@gmail.com>
2026-02-25 18:38:59 +0530

    jayasurya-BMS3-welcomedashboard

--- src/com/user/welcome/dashboard/WelcomeUI.java
+++ src/com/user/welcome/dashboard/WelcomeUI.java
@@ -1,12 +1,22 @@
 package com.user.welcome.dashboard;
 
+import java.util.Scanner;
+
 public class WelcomeUI {
 
 
 void UI() {
-	System.out.println("Welcome to our BMS");
-	// isLoggedIn(boolean isLogged){
-//		}
+	
+
+	System.out.println("1.Create Account");
+	System.out.println("2.Deposit");
+	System.out.println("3.Withdraw");
+	System.out.println("4.Check Balance");
+	System.out.println("5.Exit");
+	
+	Scanner  s1 = new Scanner(System.in);
+	System.out.println("Enter your choice");
+	int a = s1.nextInt();
 }
 
 
