ccc69cb9b6b4316f9bfb09b50f254ce9a3fcdb58
Jayasurya009 <jayasurya2k19@gmail.com>
2026-02-25 19:07:54 +0530

    updated changes request

--- src/com/user/welcome/dashboard/WelcomeUI.java
+++ src/com/user/welcome/dashboard/WelcomeUI.java
@@ -7,13 +7,11 @@ public class WelcomeUI {
 
 void UI() {
 	
-
 	System.out.println("1.Create Account");
 	System.out.println("2.Deposit");
 	System.out.println("3.Withdraw");
 	System.out.println("4.Check Balance");
 	System.out.println("5.Exit");
-	
 	Scanner  s1 = new Scanner(System.in);
 	System.out.println("Enter your choice");
 	int a = s1.nextInt();
