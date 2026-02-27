a0710e6e44df0245d704b0391c78dc362cf68d02
DK <dkboss1354@gmail.com>
2026-02-27 13:03:21 +0530

    dewansh BMS -1 landing page created

--- src/com/user/login/LandingPage.java
+++ src/com/user/login/LandingPage.java
@@ -9,10 +9,31 @@ public class LandingPage {
 	public static void main(String[] args) {
 		// TODO based on user choice re-direct to login / New user register -> Account
 		// creation
+		
 		ExistingUserLogin e = new ExistingUserLogin();
 //		LandingPage lp = new LandingPage();
 		Scanner sc = new Scanner(System.in);
-		e.loginAccTypeSel(sc, baseDBUrl);
-	}
+		boolean b=true;
+	    while(b) {
+	    	System.out.println("                      ||    WELCOME TO  BMS :-)    ||");
+			System.out.print("1. Existing Customer? \n2. New Customer? \n   Choose your option : ");
+//			Scanner sc1=new Scanner(System.in);
+	    	int a=sc.nextInt();
+			
+		    if(a==1) {
+				System.out.println("Redireting to login page...");
+				e.loginAccTypeSel(sc, baseDBUrl);
+			}
+			else if(a==2) {
+				System.out.println("Redireting to the signup page...");
+			}
+			else {
+				System.out.println("Sorry bro option unavailable, Choose one between 1 or 2");
+				b=true;
+			}	
+		    
+	    }
+	    sc.close();
+	}   
 
 }
