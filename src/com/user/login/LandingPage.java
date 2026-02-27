b9bffa5af5558d012d19af8bc57de484ffef0c09
DK <dkboss1354@gmail.com>
2026-02-27 15:24:21 +0530

    The updated version 1.0

--- src/com/user/login/LandingPage.java
+++ src/com/user/login/LandingPage.java
@@ -4,36 +4,39 @@ import java.util.Scanner;
 
 public class LandingPage {
 
-	static String baseDBUrl = "C:\\Users\\ADMIN\\eclipse-workspace\\Bankimport\\src\\assets";
+    static String baseDBUrl = "C:\\Users\\ADMIN\\eclipse-workspace\\Bankimport\\src\\assets";
 
-	public static void main(String[] args) {
-		// TODO based on user choice re-direct to login / New user register -> Account
-		// creation
-		
-		ExistingUserLogin e = new ExistingUserLogin();
-//		LandingPage lp = new LandingPage();
-		Scanner sc = new Scanner(System.in);
-		boolean b=true;
-	    while(b) {
-	    	System.out.println("                      ||    WELCOME TO  BMS :-)    ||");
-			System.out.print("1. Existing Customer? \n2. New Customer? \n   Choose your option : ");
-//			Scanner sc1=new Scanner(System.in);
-	    	int a=sc.nextInt();
-			
-		    if(a==1) {
-				System.out.println("Redireting to login page...");
-				e.loginAccTypeSel(sc, baseDBUrl);
-			}
-			else if(a==2) {
-				System.out.println("Redireting to the signup page...");
-			}
-			else {
-				System.out.println("Sorry bro option unavailable, Choose one between 1 or 2");
-				b=true;
-			}	
-		    
-	    }
-	    sc.close();
-	}   
+    public static void main(String[] args) {
 
+        ExistingUserLogin e = new ExistingUserLogin();
+        Scanner sc = new Scanner(System.in);
+        boolean b = true;
+
+        LandingPage c = new LandingPage();
+        c.landingLogic(b, sc, e);
+
+        sc.close();
+    }
+
+ 
+    public void landingLogic(boolean b, Scanner sc, ExistingUserLogin e) {
+
+        while (b) {
+            System.out.println("                    ||    WELCOME TO  BMS :-)    ||");
+            System.out.print("1. Existing Customer? \n2. New Customer? \n   Choose your option : ");
+
+            int a = sc.nextInt();
+
+            if (a == 1) {
+                System.out.println("Redirecting to login page...");
+                e.loginAccTypeSel(sc, baseDBUrl);
+            }
+            else if (a == 2) {
+                System.out.println("Redirecting to the signup page...");
+            }
+            else {
+                System.out.println("Sorry bro option unavailable, Choose one between 1 or 2");
+            }
+        }
+    }
 }
